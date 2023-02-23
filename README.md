c276477cca0ca6736142e55105bdf397d1bb4de0
SuitedIceZ <okshopm575@gmail.com>
2023-02-23 10:47:43 +0700

    add Authentication

---
+++ README.md
@@ -0,0 +1,15 @@
+# VacQ Backend server 
+for study purpose in 2110503 Software Development Practice of Chulalongkorn University only
+
+# Run Backend server
+## on dev environment
+```
+npm run dev
+```
+
+# Install dependency
+for myself
+```
+npm install <package-name> --prefix .
+```
+and add manually to node_modules dependency
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -13,8 +14,14 @@ const app = express();
 // Body parser
 app.use(express.json());
 
+// Cookie parser
+app.use(cookieParser());
+
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+
 app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
 
 app.get('/', (req, res) => {
     res.status(200).send('<h>Hello from Express</h>');
