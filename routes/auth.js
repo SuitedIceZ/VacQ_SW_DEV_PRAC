cb157fd0d5a1d89daa29c057046362a66d30e792
SuitedIceZ <okshopm575@gmail.com>
2023-04-05 23:46:06 +0700

    add logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const {register, login, getMe} = require('../controllers/auth');
+const express = require("express");
+const { register, login, getMe, logout } = require("../controllers/auth");
 
 const router = express.Router();
 
-const {protect} = require('../middleware/auth');
+const { protect } = require("../middleware/auth");
 
-router.post('/register', register);
-router.post('/login', login);
-router.get('/me', protect, getMe);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+router.get("/logout", protect, logout);
 
 module.exports = router;
